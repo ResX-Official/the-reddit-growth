20cf350f9e9dae7504aa75b89e9f7c876983b954
Fabian Amino <aminofab@gmail.com>
2024-11-09 19:39:37 +0300

    aaded

--- actions/register.ts
+++ actions/register.ts
@@ -1,4 +1,5 @@
 "use server";
+
 import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
 import bcryptjs from "bcryptjs";
@@ -6,6 +7,10 @@ import db from "@/lib/db";
 import { getUserByEmail } from "@/data/user";
 import { generateVerificationToken } from "@/lib/token";
 import { sendVerificationEmail } from "@/lib/mail";
+
+
+
+
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -32,8 +37,8 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
       password: hashedPassword,
     },
   });
-  const verificationToken = await generateVerificationToken(email);
 
+  const verificationToken = await generateVerificationToken(email);
   await sendVerificationEmail(verificationToken.email, verificationToken.token);
 
   return { success: "Confirmation Email Sent" };
