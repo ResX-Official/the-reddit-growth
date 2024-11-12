d67bcbca990ac9f29976cbc7a310a0346070e30b
Fabian Amino <aminofab@gmail.com>
2024-11-13 00:08:23 +0300

    added

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Toaster } from 'sonner';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Reddit Growth ||",
+  title: "Reddit Growth || Reddit Account Management",
   description: "Reddit Growth - Reddit Account Management",
 };
 
